71fe7c4c6b9ac6fc3d52de84fb077d4a79d656a2
Shahid Madni <shahidmadni14@gmail.com>
2026-02-28 17:53:48 +0500

    LEC 03 has been added

---
+++ LEC 03/script.js
@@ -0,0 +1,23 @@
+//lets discuss operators
+
+console.log(5+6);
+console.log(5*6);
+console.log(15/6);
+console.log(20%5);
+
+var a = 5;
+//=  this is assignment operator
+//== or === is comparison operator === means checking its data type tto be same also 
+//<, >,>=.<= these are all comparison operators
+if (a==5){
+    console.log("he is child");
+    
+}
+else if(a==4){
+    console.log("HE is chota bacha");
+    
+}
+else{
+    console.log(" bacha 5  ssaal sy bara hai");
+    
+}
