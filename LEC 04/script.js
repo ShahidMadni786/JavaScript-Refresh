fad93d886bbda374d9235a508931ea0fe798b6b5
Shahid Madni <shahidmadni14@gmail.com>
2026-03-01 13:21:57 +0500

    lec 04 is added

---
+++ LEC 04/script.js
@@ -0,0 +1,64 @@
+//loops and strings 
+for(let i=1; i<=115; i++){
+    console.log("Shahid bhai is loop ki trah consistent ho jao");
+    
+}
+for(let i=1; i<=50; i++){
+    console.log(i);
+    
+}
+for(let i=100; i>=1; i--){
+    console.log(i);
+    
+}
+
+for(let i=0; i<=20; i++){
+   if(i%2==0){
+    console.log(i);
+    
+   }
+    
+}
+for(let i=1; i<=50; i++){
+   if(i%5==0){
+    console.log(i);
+    
+   }
+    
+}
+count =0;
+for(let i=1; i<=100; i++){
+
+   if(i%3==0){
+       count++
+    
+   }
+    
+}
+console.log(count);
+//basic while loop syntax
+let i =0; //initialization
+while(i<=786){  //condition
+    console.log(i);
+    i++   //updation
+    
+}
+
+//do while loop
+let b=0;
+do{
+    console.log("main babbar hoon");
+
+    b++
+}while (b<=50)
+
+
+//Now Stringss let discuss the Stringss Now 
+//combination of characters Called Strings
+
+let myname = " Shahid";
+let myfullname = `This is
+my 
+name
+is babbar`
+console.log(`${myname}${myfullname}`);
