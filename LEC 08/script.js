eb54aa65274c900c10dec558e1771d5300ab72c6
Shahid Madni <shahidmadni14@gmail.com>
2026-03-02 16:05:43 +0500

    variable scoping and Temporal Dead Zone

---
+++ LEC 08/script.js
@@ -0,0 +1,9 @@
+//variable scoping and what is temporal dead zone
+//Global scope
+var age =25;
+//we can  use it anywhere
+//temporal dead zone means let and var cant be accepted by the term hoisting
+console.log(myname); //from this to this
+console.log("heloo g");
+console.log("heloo g");
+let myname = "Shahid";   //temporal deadzone
