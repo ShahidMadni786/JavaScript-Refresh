c535722d117e2571a9b907a4b9fcd7e267fd625e
Shahid Madni <shahidmadni14@gmail.com>
2026-03-07 12:57:07 +0500

    fetch api added

---
+++ LEC 13/script.js
@@ -0,0 +1,8 @@
+async function getdata (){
+     let response = await fetch("https://jsonplaceholder.typicode.com/posts");
+     let data = await response.json();
+     console.log(data[1].body);
+     
+}
+
+getdata()
